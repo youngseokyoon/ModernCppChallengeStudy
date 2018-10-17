84ccf9a4d4731f387af575235e9b47c431c15a56
youngseok <earwigz32@gmail.com>
2018-10-18 00:37:24 +0900

    math: Sum of naturals divisible by 3 and 5
    Signed-off-by: youngseok <earwigz32@gmail.com>

--- math/Problem1/main.cpp
+++ math/Problem1/main.cpp
@@ -1,6 +1,18 @@
-#include <gsl/gsl>
+#include <stdint.h>
+#include <string>
 
+using namespace std;
 int main(int argc, char* argv[])
 {
+    uint32_t limit = atoi(argv[1]);
+    uint64_t sum = 0U;
+    for (int32_t index = 0U; index <= limit; index++) {
+        if ((index % 3U == 0U) || (index % 5U == 0U)) {
+            sum += index;
+        }
+    }
+
+    printf("%lu\n", sum);
+
     return 0;
 }
